83e4a46289ca7efc327d3d24d31d9ad9a7ed26f5
sodastereo <f_er_@hotmail.com>
2023-11-29 13:38:31 -0500

    bootstrap added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,72 @@
 import React from 'react';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
-    </div>
+    <Container>
+      <div className="App">
+        {/* "Document Upload" section */}
+        <header className="App-header">
+          {/* Your header content */}
+        </header>
+
+        <Row>
+          {/* First column */}
+          <Col>
+            {/* "Select Import Name" section */}
+            <Row>
+              <Col>
+                <div>
+                  text
+                </div>
+              </Col>
+            </Row>
+
+            {/* "Drag & Drop" section */}
+            <Row>
+              <Col>
+                <div>
+                  text
+                </div>
+              </Col>
+            </Row>
+          </Col>
+
+          {/* Second column */}
+          <Col>
+            {/* "Split schedule" section */}
+            <Row>
+              <Col>
+                <div>
+                  text
+                </div>
+              </Col>
+            </Row>
+            
+            {/* "Location Checking" section */}
+            <Row>
+              <Col>
+                <div>
+                  text
+                </div>
+              </Col>
+            </Row>
+
+            {/* "Continue import" section */}
+            <Row>
+              <Col>
+                <div>
+                  text
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
+    </Container>
   );
 }
 
